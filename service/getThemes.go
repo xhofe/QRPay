c92899259fff7d0dd09fb4a673ba200d8b0f0570
微凉 <927625802@qq.com>
2021-06-22 20:06:19 +0800

    :bug: 获取主题

--- service/getThemes.go
+++ service/getThemes.go
@@ -1,9 +1,12 @@
 package service
 
-import "io/ioutil"
+import (
+	"github.com/Xhofe/QRPay/views"
+)
 
 func GetThemes() []string {
-	files, _ := ioutil.ReadDir("./views/themes")
+	//files, _ := ioutil.ReadDir("./views/themes")
+	files, _ := views.Views.ReadDir("themes")
 	themes := make([]string,0)
 	for _,f := range files {
 		name := f.Name()
